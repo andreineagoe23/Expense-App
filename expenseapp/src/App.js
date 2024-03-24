47d59ce69d8a3ee7597279ea2e1be7c45aeb0c05
andreineagoe23 <you@example.com>
2024-03-24 13:05:06 +0000

    initial changes

--- expenseapp/src/App.js
+++ expenseapp/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import Login from "./Login/login.js";
+import RegisterForm from "./Register/register.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className="middle-buttons">
+            <button>Button 1</button>
+            <button>Button 2</button>
+            <button>Button 3</button>
+            <button>Button 4</button>
+          </div>
+          <div className="right-buttons">
+            <Link to="./Login/login.js">
+              <button>Login</button>
+            </Link>
+            <Link to="./Register/register.js">
+              <button>Become a member</button>
+            </Link>
+          </div>
+        </header>
+      </div>
+      <Routes>
+        <Route path="./Login/login.js" component={Login} />
+      </Routes>
+      <Routes>
+        <Route path="./Register/register.js" component={RegisterForm} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ expenseapp/src/Register/register.js
@@ -0,0 +1,63 @@
+import React from "react";
+class RegisterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+    };
+  }
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // Add your register logic here
+    // You can access the form data using this.state
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
+
+// Export the register form component
+export default RegisterForm;
