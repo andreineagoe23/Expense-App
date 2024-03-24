7868a98449450350291554f3a5563c62396dbb9c
andreineagoe23 <you@example.com>
2024-03-24 15:13:12 +0000

    initial header

--- expenseapp/src/App.js
+++ expenseapp/src/App.js
@@ -1,36 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import Navbar from "./navbar.js";
 import Login from "./Login/login.js";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterForm from "./Register/register.js";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <div className="middle-buttons">
-            <button>Button 1</button>
-            <button>Button 2</button>
-            <button>Button 3</button>
-            <button>Button 4</button>
-          </div>
-          <div className="right-buttons">
-            <Link to="./Login/login.js">
-              <button>Login</button>
-            </Link>
-            <Link to="./Register/register.js">
-              <button>Become a member</button>
-            </Link>
-          </div>
-        </header>
+        <Navbar />
       </div>
       <Routes>
-        <Route path="./Login/login.js" component={Login} />
+        <Route path="/login.js" element={<Login />} />
+        <Route path="/register.js" element={<RegisterForm />} />
       </Routes>
-      <Routes>
-        <Route path="./Register/register.js" component={RegisterForm} />
-      </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
---
+++ expenseapp/src/navbar.js
@@ -0,0 +1,45 @@
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <div className="navbar-left">
+          <a href="App.js">
+            <img src="./Images/logo.jpg" alt="Logo" />
+          </a>
+        </div>
+
+        <div className="navbar-middle">
+          <ul id="navbar">
+            <li>
+              <a href="App.js">Home</a>
+            </li>
+            <li>
+              <a href="App.js">Training Course</a>
+            </li>
+            <li>
+              <a href="App.js">Expense</a>
+            </li>
+            <li>
+              <a href="App.js">Support</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="navbar-right">
+          <ul id="login">
+            <li>
+              <a href="/login.js">Login</a>
+            </li>
+            <li>
+              <a href="/register.js">Become a member</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
