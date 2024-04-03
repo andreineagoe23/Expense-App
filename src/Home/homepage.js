26722e0cffe57cbc2002b8970656edd60869efa7
andreineagoe23 <you@example.com>
2024-04-03 12:11:47 +0100

    chnges to the home page

--- src/Home/homepage.js
+++ src/Home/homepage.js
@@ -1,5 +1,10 @@
+// homepage.js
 import React from "react";
 import "./homepage.css";
+import { ReactComponent as TrainingIcon } from "./vector.svg"; // Import your SVG icons
+import { ReactComponent as ExpenseIcon } from "./vector.svg";
+import { ReactComponent as SupportIcon } from "./vector.svg";
+import GraphIcon from "./graph.svg";
 
 const HomePage = () => {
   return (
@@ -17,13 +22,40 @@ const HomePage = () => {
         </div>
         <div className="right-section">
           <h2>Expense Overview</h2>
-          <img src="/graph.svg" alt="Expense Graph" />
+          <img src={GraphIcon} alt="Expense Graph" />
         </div>
       </div>
       <div className="footer">
-        <button>Training Course</button>
-        <button>Expense Management</button>
-        <button>Support</button>
+        <button>
+          <TrainingIcon className="button-icon" />
+          <span className="button-text">
+            Training Course
+            <br />
+            <span className="button-subtext">
+              12 fresh courses are waiting! Let's start now.
+            </span>
+          </span>
+        </button>
+        <button>
+          <ExpenseIcon className="button-icon" />
+          <span className="button-text">
+            Expense Management
+            <br />
+            <span className="button-subtext-small">
+              15% more this week. See details here.
+            </span>
+          </span>
+        </button>
+        <button>
+          <SupportIcon className="button-icon" />
+          <span className="button-text">
+            Support
+            <br />
+            <span className="button-subtext">
+              Need extra? Reach out to us now.
+            </span>
+          </span>
+        </button>
       </div>
     </div>
   );
