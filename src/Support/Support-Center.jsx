76a38c3e91a8b033f1e7aa00c3a995434f4a65c2
tiago <ec22448@qmul.ac.uk>
2024-04-08 14:40:44 +0100

    CSS fixed

--- src/Support/Support-Center.jsx
+++ src/Support/Support-Center.jsx
@@ -13,21 +13,10 @@ const SupportCenter = () => {
                 <div className="support-text">
                     <h1>Welcome to the Support Center</h1>
                 </div>
-                <div className="contact-text">
-                    <p>Reach out to us via email through</p>
-                </div>
-                <div className="contact-text2">
-                    <p>the contact form to the right for support.</p>
-                </div>
                 <div className="contact-info-email">
-                    <img src={emailImg} alt='Email' className='email-Icon'/>
                     <p>expenseapp@gmail.com</p>
                 </div>
 
-                <div className="contact-info-location">
-                    <img src={locationImg} alt='Person' className='Location-Icon'/>
-                    <p>Mile End, London</p>
-                </div>
             </div>
             <form className="contact-form">
                 <div className="form-group">
@@ -42,11 +31,13 @@ const SupportCenter = () => {
                     <label htmlFor="message">Message:</label>
                     <textarea id="message" name="message" rows="4"/>
                 </div>
-                <button type="button" onClick={() => window.location.href = '/SupportManagement.jsx'}>Submit</button>
+                <button type="button" className="submit-button"
+                        onClick={() => window.location.href = '/SupportManagement.jsx'}>Submit
+                </button>
             </form>
             <img src={supportBubble} alt='Bubble' className='Support-Bubble'/>
             <img src={supportPerson} alt='Person' className='Support-Person'/>
-            <img src={emailImg} alt='Person' className='email-Icon'/>
+            <img src={emailImg} alt='Person' className='Email-Icon'/>
             <Link to="/SupportHelp">
                 <button className="Help-Button">View Help Articles</button>
             </Link>
--- src/Support/Support-Help.jsx
+++ src/Support/Support-Help.jsx
@@ -13,47 +13,43 @@ const SupportHelp = () => {
                 <div className="support-text">
                     <h1>Welcome to the Support Center</h1>
                 </div>
-                <div className="contact-text">
-                    <p>Reach out to us via email through</p>
-                </div>
-                <div className="contact-text2">
-                    <p>the contact form to the right for support.</p>
-                </div>
                 <div className="contact-info-email">
-                    <img src={emailImg} alt='Email' className='email-Icon'/>
                     <p>expenseapp@gmail.com</p>
-                </div>
-
-                <div className="contact-info-location">
-                    <img src={locationImg} alt='Person' className='Location-Icon'/>
-                    <p>Mile End, London</p>
                 </div>
             </div>
 
-            <img src={supportBubble} alt='Bubble' className='Support-Bubble'/>
-            <img src={supportPerson} alt='Person' className='Support-Person'/>
-            <img src={emailImg} alt='Person' className='email-Icon'/>
+            <img src={supportBubble} alt='Bubble' className='Support-Bubble2'/>
+            <img src={supportPerson} alt='Person' className='Support-Person2'/>
+            <img src={emailImg} alt='Person' className='Email-Icon2'/>
             <Link to="/SupportCenter">
                 <button className="Center-Button">Go to Support Center</button>
             </Link>
 
             <div className="boxes">
-                <div className="box">
-                    <h2>Title 1</h2>
-                    <p>Description 1</p>
-                </div>
-                <div className="box">
-                    <h2>Title 2</h2>
-                    <p>Description 2</p>
-                </div>
-                <div className="box">
-                    <h2>Title 3</h2>
-                    <p>Description 3</p>
-                </div>
-                <div className="box">
-                    <h2>Title 4</h2>
-                    <p>Description 4</p>
-                </div>
+                <Link to="/box1">
+                    <div className="box">
+                        <h2>How to Login/Register</h2>
+                        <p>Description 1</p>
+                    </div>
+                </Link>
+                <Link to="/box2">
+                    <div className="box">
+                        <h2>How to create an expense Form</h2>
+                        <p>Description 2</p>
+                    </div>
+                </Link>
+                <Link to="/box3">
+                    <div className="box">
+                        <h2>Title 3</h2>
+                        <p>Description 3</p>
+                    </div>
+                </Link>
+                <Link to="/box4">
+                    <div className="box">
+                        <h2>Title 4</h2>
+                        <p>Description 4</p>
+                    </div>
+                </Link>
             </div>
         </div>
     );
