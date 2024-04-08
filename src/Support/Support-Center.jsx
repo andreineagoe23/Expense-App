091121275dfb919add82ee6f5a930b3e5894bdc3
Andrei <you@example.com>
2024-04-08 11:43:22 +0100

    .

--- src/Support/Support-Center.jsx
+++ src/Support/Support-Center.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Add this import statement
+import { Link } from "react-router-dom";
 import supportBubble from './SupportBubble.png';
 import supportPerson from './SupportPerson.png';
 import './SupportCenter.css';
-import locationImg from './location-icon.png'
-import emailImg from './email-icon.png'
 
 const SupportCenter = () => {
     return (
@@ -13,22 +11,13 @@ const SupportCenter = () => {
                 <div className="support-text">
                     <h1>Welcome to the Support Center</h1>
                 </div>
-                <div className="contact-text">
-                    <p>Reach out to us via email through</p>
-                </div>
-                <div className="contact-text2">
-                    <p>the contact form to the right for support.</p>
-                </div>
-                <div className="contact-info-email">
-                    <img src={emailImg} alt='Email' className='email-Icon'/>
-                    <p>expenseapp@gmail.com</p>
-                </div>
-
-                <div className="contact-info-location">
-                    <img src={locationImg} alt='Person' className='Location-Icon'/>
-                    <p>Mile End, London</p>
-                </div>
             </div>
+
+           
+  <img src={supportBubble} alt='Bubble' className='Support-Bubble'/>
+  <img src={supportPerson} alt='Person' className='Support-Person'/>
+
+
             <form className="contact-form">
                 <div className="form-group">
                     <label htmlFor="fullName">Full Name:</label>
@@ -44,13 +33,10 @@ const SupportCenter = () => {
                 </div>
                 <button type="button" onClick={() => window.location.href = '/SupportManagement.jsx'}>Submit</button>
             </form>
-            <img src={supportBubble} alt='Bubble' className='Support-Bubble'/>
-            <img src={supportPerson} alt='Person' className='Support-Person'/>
-            <img src={emailImg} alt='Person' className='email-Icon'/>
+        
             <Link to="/SupportHelp">
                 <button className="Help-Button">View Help Articles</button>
             </Link>
-
         </div>
     );
 }
